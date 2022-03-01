7fc0a4c776ef8aac59a63e1a74a72f0de2c8ac1d
Xanthus <Xanthus58@protonmail.com>
2022-03-02 04:43:17 +1030

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Seven-Bot
+ Its a choose your own adventure story bot that was made for fun with my partner @Wolfen_XVII
---
+++ Run Me First.bat
@@ -0,0 +1,5 @@
+REM installs all of the files needed for the affirmations program
+winget install 9PJPW5LDXLZ5
+pip install --upgrade pip
+pip install keyboard
+pip install colorama
